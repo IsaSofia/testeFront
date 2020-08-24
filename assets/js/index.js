941a04fc2aa3e522bf8c0fe98322ef1f9d8bebdf
BelaMarques <isasofiamarx@gmail.com>
2020-08-23 22:01:03 -0300

    Add: Função para verificar a senha e função verificar o checkbox

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// ============== Máscaras dos inputs ==============
+// Máscaras dos inputs
 $('.cpf-mask').mask('999.999.999-99', {
   clearIfNotMatch: true
 });
@@ -17,5 +17,24 @@ $('.tel-mask').mask('(99) 99999-9999', {
       pattern: /[0-9]/,
       optional: true
     }
+  }
+});
+
+// Verifica se senhas são iguais
+function validarSenha(senha, senhaConfirma)
+{
+    var senha1 = document.getElementById(senha).value;
+    var senha2 = document.getElementById(senhaConfirma).value;
+		
+    if (senha1 != senha2)
+    {
+      	alert('Senhas diferentes!');
+    }
+}
+
+// Verificar se o checkBox foi selecionado
+$('.btn-cadastrar').click(function() {
+  if ($('.form-check-input').is(':checked') === false) {
+    alert('Marque o checkbox!');
   }
 });
